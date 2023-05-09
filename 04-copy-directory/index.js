c178e26dab7bf06e493af64a7e36b553d698bc0d
DonStacky <117210841+DonStacky@users.noreply.github.com>
2023-05-09 12:46:27 +0300

    fix: delete old files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,19 +1,30 @@
 const fs = require('fs');
 const path = require('path');
-const { stdout } = process;
 
-fs.mkdir('./04-copy-directory/files-copy', {recursive: true}, (err) => {
-  err ? console.log(err.message) : null;
+fs.access('./04-copy-directory/files-copy', error => {
+  if (!error) {
+  fs.rm('./04-copy-directory/files-copy', {recursive: true}, (err) => {
+    err ? console.log(err.message) : null;
+    makeCopy();
+  })
+  } else {
+    makeCopy();
+  }
 });
 
-fs.readdir(path.join(__dirname, './files'), {withFileTypes: true}, (err, files) => {
-  if (err) console.log(err.message);
-  for (let file of files) {
-      if (err) console.log(err.message);
-      if (file.isFile()) {
-        fs.copyFile(path.join(__dirname, `./files/${file.name}`), path.join(__dirname, `./files-copy/${file.name}`), err => {
-          if (err) console.log(err.message);
-        })
-      }
-  }
-})
+function makeCopy() {
+  fs.mkdir('./04-copy-directory/files-copy', {recursive: true}, (err) => {
+    err ? console.log(err.message) : null;
+  });
+  fs.readdir(path.join(__dirname, './files'), {withFileTypes: true}, (err, files) => {
+    if (err) console.log(err.message);
+    for (let file of files) {
+        if (err) console.log(err.message);
+        if (file.isFile()) {
+          fs.copyFile(path.join(__dirname, `./files/${file.name}`), path.join(__dirname, `./files-copy/${file.name}`), err => {
+            if (err) console.log(err.message);
+          })
+        }
+    }
+  })
+}
